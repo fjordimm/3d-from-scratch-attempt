902794ab1d3e833595dab76200d7e53698b6826a
fjordimm <fjordimm@gmail.com>
2024-07-09 17:56:55 -0700

    added another script for compiling without doing the entire cmake build

--- WindowTestProject/libs/WindowTest/src/WindowTest/main.cpp
+++ WindowTestProject/libs/WindowTest/src/WindowTest/main.cpp
@@ -10,7 +10,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hi this is my test window", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
