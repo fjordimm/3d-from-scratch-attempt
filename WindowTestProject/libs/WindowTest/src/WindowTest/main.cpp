cb5f1b5481cee1e9a88041d3d1f186e7703ffe11
fjordimm <fjordimm@gmail.com>
2024-07-11 16:44:56 -0700

    got glew to work

--- WindowTestProject/libs/WindowTest/src/WindowTest/main.cpp
+++ WindowTestProject/libs/WindowTest/src/WindowTest/main.cpp
@@ -1,8 +1,14 @@
 
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <stdio.h>
 
 int main(void)
 {
+	printf("helooooooo\n");
+
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -10,7 +16,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hi this is my test window", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hiiii this is my test window", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -20,17 +26,41 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	/* Initialize GLEW */
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// GLuint vbo1;
+	// glGenBuffers(1, &vbo1);
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	printf("%u\n", vertexBuffer);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+		/* Process inputs */
 
-        /* Swap front and back buffers */
+        /* Render stuff */
+
+		glClearColor(0.4f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/////////////////////////////////////////////////
+		{
+			float vertices[] =
+			{
+				0.0f, 0.5f,
+				0.5f, -0.5f,
+				-0.5f, -0.5f
+			};
+		}
+		/////////////////////////////////////////////////
+
+        /* Do stuff required by GLFW */
+
+		glfwPollEvents();
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
     }
 
     glfwTerminate();
